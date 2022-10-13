c888e12d270ff34698781f3846afccbce97a5834
Khidayat_Iminova <hidayatka89@gmail.com>
2022-10-13 22:38:38 +0700

    Clean Task5-5

--- HW5/Homework5/Task_5-5.py
+++ HW5/Homework5/Task_5-5.py
@@ -1,7 +1,1 @@
-# Function for a single game of Tic-Tac-Toe 
-def singlegame(curplayer): 
-    # Representing the Tic-Tac-Toe 
-    val = [' ' for i in range(9)] 
-      
-    # Storing the positions occupied by X and O 
-    playerpos = {'X' : [], 'O' : []} 
+# Создайте программу для игры в ""Крестики-нолики"".
